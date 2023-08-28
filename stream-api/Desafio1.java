782c9940685581762d8d244c564c489ba9d07daa
Francine Hahn <fran_hahn@hotmail.com>
2023-08-28 15:46:49 -0300

    feat: desafio 1

--- stream-api/Desafio1.java
+++ stream-api/Desafio1.java
@@ -1,13 +1,18 @@
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Desafio1 {
     List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
     public void ordenarArray () {
-        Collections.sort(numeros);
-        System.out.println(numeros);
+        List<Integer> numerosOrdenados = numeros
+        .stream()
+        .sorted(Comparator.naturalOrder())
+        .collect(Collectors.toList());
+
+        System.out.println(numerosOrdenados);
     }
 
     public static void main(String[] args) {
