d786e09d96a4148fd644a258a951afc787663a56
Francine Hahn <fran_hahn@hotmail.com>
2023-09-05 14:50:35 -0300

    feat: singleton

---
+++ web-api/WebApiJpaApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiJpaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiJpaApplication.class, args);
+	}
+
+}
