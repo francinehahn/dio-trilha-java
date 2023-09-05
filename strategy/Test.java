bf81391730705a96f5c543a57b63bc53cf4c4a85
Francine Hahn <fran_hahn@hotmail.com>
2023-09-05 15:06:23 -0300

    feat: strategy

---
+++ strategy/Test.java
@@ -0,0 +1,20 @@
+//Padrão de projeto Strategy
+
+public class Test {
+    public static void main(String[] args) {
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(agressivo);
+        robo.mover();
+
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+    }
+}
